19144a519db5f4e74301e4d61e130cda3ffcfbbc
Elturan <elturanfcb@gmail.com>
2023-10-11 09:54:23 +0400

    create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,12 @@ import About from './pages/about/About'
 import SignIn from './pages/sign-in/SignIn'
 import Profile from './pages/profile/Profile'
 import SignUp from './pages/sign-up/SignUp'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
