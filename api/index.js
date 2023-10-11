4bd44047ae7aefe60b34f1f98b7a5974d5d85c66
Elturan <elturanfcb@gmail.com>
2023-10-11 17:32:12 +0400

    complete sign up functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv"
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js"
+import cors from "cors"
 
 dotenv.config()
 
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.use("/api/user", userRouter)
@@ -29,6 +31,6 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000")
+app.listen(8000, () => {
+    console.log("Server is running on port 8000")
 })
--- client/src/pages/sign-up/SignUp.jsx
+++ client/src/pages/sign-up/SignUp.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
 
 export default function SignUp() {
+  const [postData, setPostData] = useState({})
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const navigate=useNavigate()
+
+  const handleChange = (e) => {
+    setPostData({ ...postData, [e.target.id]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    axios.post(`/api/auth/signup`, postData)
+      .then(res => {
+        setLoading(false)
+        setError("")
+        if(res.status==201){
+          navigate("/sign-in")
+        }
+      })
+      .catch(err => {
+        setLoading(false)
+        setError("Username or email already using")
+      })
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form className='flex flex-col gap-4 '>
         <input type="text" placeholder='username' className='border p-3 rounded-lg'
-          id='username' />
+          id='username' onChange={handleChange} />
         <input type="email" placeholder='email' className='border p-3 rounded-lg'
-          id='email' />
+          id='email' onChange={handleChange} />
         <input type="password" placeholder='password' className='border p-3 rounded-lg'
-          id='password' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+          id='password' onChange={handleChange} />
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80' onClick={handleSubmit}>{loading ? 'loading...' : 'Sign up'}</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
@@ -20,6 +47,7 @@ export default function SignUp() {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500'>{error}</p>}
     </div>
   )
 }
