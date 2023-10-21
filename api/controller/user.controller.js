f10f6b5bfb8d4cf9104e7cb91bb700c8d95dfd22
Elturan <elturanfcb@gmail.com>
2023-10-21 12:46:06 +0400

    create user listing API

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from "bcryptjs"
 import User from "../models/user.model.js"
+import Listing from "../models/listing.model.js"
 
 export const updateUser = async (req, res) => {
     try {
@@ -28,6 +29,16 @@ export const deleteUser = async (req, res) => {
         await User.findByIdAndDelete(req.params.id)
         res.clearCookie('access_token')
         res.status(200).json("User has been deleted.")
+    } catch (error) {
+        res.status(500).json('Internal Server Error')
+    }
+}
+
+export const getUserListings = async (req, res) => {
+    if (req.user.id !== req.params.id) return res.status(401).json('You can only view your own listings!')
+    try {
+        const listings = await Listing.find({ userRef: req.params.id })
+        res.status(200).json(listings)
     } catch (error) {
         res.status(500).json('Internal Server Error')
     }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { deleteUser, updateUser } from "../controller/user.controller.js";
+import { deleteUser, getUserListings, updateUser } from "../controller/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
